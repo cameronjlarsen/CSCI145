8f1493cbee0f7fc8d30209c859450760753125fc
Cameron Larsen <camjlarsen@gmail.com>
2020-10-29 23:10:41 -0700

    Reorganized project files

---
+++ Project2/LetterGrade.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class LetterGrade {
+
+    public static char scoreToLetterGrade(int score){
+        char grade = 'F';
+        if (score >= 90)
+            grade = 'A';
+        else if (score >= 80 && score < 90)
+            grade = 'B';
+        else if (score >= 70 && score < 80)
+            grade = 'C';
+        else if (score >= 60 && score < 70)
+            grade = 'D';
+        else if(score >=0 && score < 60)
+            grade = 'F';
+        else 
+            grade =' ';
+        return grade;
+    } 
+    public static boolean isGrading(int score){
+        if (score == -1)
+            return false;
+        else return true;
+    }
+    public static void main(String[] args){
+
+        System.out.println("Welcome to the letter grader! Input the score to receive the letter grade!");
+        Scanner input = new Scanner(System.in);
+        int score; 
+        do{
+            System.out.print("Please input the test score (-1 to stop entry): ");
+            score = input.nextInt();
+            if(score >= 0)
+                System.out.printf("Letter grade is %s\n", scoreToLetterGrade(score));
+            else
+                System.out.println("Done grading...");
+        }while(isGrading(score));
+        input.close();
+    }
+    
+}
--- Project2/PascalTriangle.java
+++ Project2/PascalTriangle.java
@@ -1,5 +1,3 @@
-package PascalTriangle;
-
 import java.util.Scanner;
 
 public class PascalTriangle{
